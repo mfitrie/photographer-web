c715760f181ee48b8bacdb7af720085196c90b4e
Fitrie <mfitrie78@gmail.com>
2026-05-29 18:43:02 +0800

    build: update next config, remove github action

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.0.148'],
+  output: 'export',
+  trailingSlash: true,      // recommended for static hosting
+  images: {
+    unoptimized: true,      // next/image needs this for static export
+  },
 };
 
 export default nextConfig;

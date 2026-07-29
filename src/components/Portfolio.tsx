5ba34afe7b8f7e07eb9366bdd3609d88e83588bc
Fitrie <mfitrie78@gmail.com>
2026-07-29 21:00:05 +0800

    feat: update style portfolio

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -8,7 +8,6 @@ export default function Portfolio() {
     return (
         <section
             id="portfolio"
-            className="container py-24 sm:py-32"
         >
             <h2 className="text-3xl md:text-4xl font-bold text-center mb-10">
                 <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">Portfolio</span>
